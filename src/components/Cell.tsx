78e89b5135d551671e4b54a1ac15081fe92130d6
AtsuH27 <hongzhonga78@gmail.com>
2024-02-05 22:37:54 +0900

    OseroGame

---
+++ src/components/Cell.tsx
@@ -0,0 +1,25 @@
+type CellProps={
+    content:string;
+    onClick:()=>void;
+    puttable:boolean;
+};
+
+const Cell: React.FC<CellProps>=(props)=>{
+    return(
+        <div
+            style={{
+                width:"50px",
+                height:"50px",
+                border:"1px solid black",
+                justifyContent:"center",
+                alignItems:"center",
+                backgroundColor:props.puttable?"green":"white",
+            }}
+            onClick={props.onClick}
+        >
+            {props.content}
+        </div>
+    );
+};
+
+export default Cell;
